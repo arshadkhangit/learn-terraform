138619f25f0a2bce0a30ea0abac60f789bae483a
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-06-15 18:43:16 +0530

    provisioner

--- 08-provisioner/main.tf
+++ 08-provisioner/main.tf
@@ -1,15 +1,14 @@
 resource "aws_instance" "web" {
-  ami = data.aws_ami.example.id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.example.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-08e7c5df7f282f710"]
 
   tags = {
     Name = "Hello World"
   }
-}
 
 
-  provisioner "remote-exec"{
+  provisioner "remote-exec" {
     connection {
       type     = "ssh"
       user     = "centos"
@@ -17,13 +16,14 @@ resource "aws_instance" "web" {
       host     = self.public_ip
     }
     inline = [
-       "sudo labauto ansible",
-        "ansible-pull -i localhost, -u https://github.com/arshadkhangit/roboshop-ansible main.yml -e env=dev -e rol_name=frontend"
-     ]
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -u https://github.com/arshadkhangit/roboshop-ansible main.yml -e env=dev -e rol_name=frontend"
+    ]
+  }
+}
+  data "aws_ami" "example" {
+    owners      = [973714476881]
+    most_recent = true
+    name_regex  = "Centos-8-DevOps-Practice"
   }
 
-data "aws_ami" "example"{
-  owners = [973714476881]
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-}
