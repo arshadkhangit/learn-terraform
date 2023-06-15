1858eae27c84708f7718d91792f557903a022ad9
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-06-15 14:31:58 +0530

    terraform variables plain list map

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -60,9 +60,9 @@ output "course" {
 }
 
 output "courses" {
-  value = var.courses
+  value = var.courses[2]
 }
 
 output "course_details" {
-  value = var.course_details.devops
+  value = var.course_details["devops"]
 }
