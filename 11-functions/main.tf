9077c1a76e7e7b5f857272ea870d813e31249d8f
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-06-16 18:18:27 +0530

    functions

--- 11-functions/main.tf
+++ 11-functions/main.tf
@@ -5,3 +5,10 @@ variable "class" {
 output "class" {
   value = upper(var.class)
 }
+
+variable "fruits" {
+  default = ["apple", "banana", "orange"]
+}
+output "fruit-count" {
+  value = length(var.fruits)
+}
