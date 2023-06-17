1ddf9702070b87dfb68264bd28ec459e71811fdd
arshadgit <86900957+arshadgit@users.noreply.github.com>
2023-06-17 10:25:41 +0530

    loop

--- 08-provisioner/main.tf
+++ 08-provisioner/main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "web" {
   }
 }
   data "aws_ami" "example" {
-    owners      = [973714476881]
+    owners      = ["973714476881"]
     most_recent = true
     name_regex  = "Centos-8-DevOps-Practice"
   }
